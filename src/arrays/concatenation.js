0b1191cb9aa2a9d99d95195693688969ee9e50d3
Ali Yusuf <w2bhnp@gmail.com>
2018-09-19 14:28:36 +0600

    use predicate

--- src/arrays/concatenation.js
+++ src/arrays/concatenation.js
@@ -1,8 +1,9 @@
 let countries = ['Bangladesh', 'India', 'Pakistan'];
 let saarcCountries = countries.concat('Maldives', 'Bhutan', 'Nepal', 'Afghanistan', 'Sri Lanka');
 let echo = val => console.log(val);
+let makeUpperCase = val => val.toUpperCase();
 // saarcCountries.forEach( val => echo(val));
 
 // map
-let upperCasedSaarcCountries = saarcCountries.map(country => country.toUpperCase());
+let upperCasedSaarcCountries = saarcCountries.map(makeUpperCase);
 upperCasedSaarcCountries.forEach(echo);
