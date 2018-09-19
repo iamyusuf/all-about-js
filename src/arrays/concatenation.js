77c05e2a4f9df4234dccd8024cc2b85e96204b97
Ali Yusuf <w2bhnp@gmail.com>
2018-09-19 14:26:05 +0600

    array map

--- src/arrays/concatenation.js
+++ src/arrays/concatenation.js
@@ -1,4 +1,8 @@
 let countries = ['Bangladesh', 'India', 'Pakistan'];
 let saarcCountries = countries.concat('Maldives', 'Bhutan', 'Nepal', 'Afghanistan', 'Sri Lanka');
 let echo = val => console.log(val);
-saarcCountries.forEach( val => echo(val));
+// saarcCountries.forEach( val => echo(val));
+
+// map
+let upperCasedSaarcCountries = saarcCountries.map(country => country.toUpperCase());
+upperCasedSaarcCountries.forEach(echo);
